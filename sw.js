5e65594afda448ea944e5c38759720ca47735040
Simon <ihemesimon@gmail.com>
2026-08-30 04:37:38 +0100

    V 4.2

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@ const ASSETS = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
+      return cache.addAll(ASSETS);
     })
   );
 });
